7df64390f01229d9c6929da03c9e13a713357608
Alejandro Sellero <alejandro.s@monoqi.de>
2015-10-06 10:17:35 +0200

    Update 2015-10-06T08:17:33.441Z

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-var React = require('react'),
-    Conditional = require('react-conditions');
+var React = require('react');
+
+var _require = require('react-conditions');
+
+var Conditional = _require.Conditional;
+var If = _require.If;
 
 var App = React.createClass({
 	displayName: 'App',
@@ -11,6 +15,46 @@ var App = React.createClass({
 		return React.createElement(
 			'div',
 			null,
+			React.createElement(
+				'p',
+				null,
+				'Input:',
+				React.createElement('br', null),
+				React.createElement(
+					'pre',
+					{ className: 'code' },
+					'\t<If test={true}>\n\t\t<span>Hello</span>\n\t</If>'
+				),
+				'Output:',
+				React.createElement('br', null),
+				React.createElement(
+					If,
+					{ test: true },
+					React.createElement(
+						'span',
+						null,
+						'Hello'
+					)
+				)
+			),
+			React.createElement(
+				'p',
+				null,
+				'Input:',
+				React.createElement('br', null),
+				React.createElement(
+					'pre',
+					{ className: 'code' },
+					'\t<If test={true}>\n\t\tHello\n\t</If>'
+				),
+				'Output:',
+				React.createElement('br', null),
+				React.createElement(
+					If,
+					{ test: true },
+					'Hello'
+				)
+			),
 			React.createElement(
 				'p',
 				null,
@@ -88,6 +132,42 @@ var App = React.createClass({
 					React.createElement(
 						Conditional.ElseIf,
 						{ test: false },
+						'Hello2'
+					),
+					React.createElement(
+						Conditional.Else,
+						null,
+						'Else'
+					)
+				)
+			),
+			React.createElement(
+				'p',
+				null,
+				'Input:',
+				React.createElement('br', null),
+				React.createElement(
+					'pre',
+					{ className: 'code' },
+					'\t<Conditional>\n\t\t<Conditional.If test={() => 1 === 2}>\n\t\t\tHello\n\t\t</Conditional.If>\n\t\t<Conditional.ElseIf test={() => 1 === 1}>\n\t\t\tHello2\n\t\t</Conditional.ElseIf>\n\t\t<Conditional.Else >\n\t\t\tElse\n\t\t</Conditional.Else>\n\t</Conditional>'
+				),
+				'Output:',
+				React.createElement('br', null),
+				React.createElement(
+					Conditional,
+					null,
+					React.createElement(
+						Conditional.If,
+						{ test: function () {
+								return 1 === 2;
+							} },
+						'Hello'
+					),
+					React.createElement(
+						Conditional.ElseIf,
+						{ test: function () {
+								return 1 === 1;
+							} },
 						'Hello2'
 					),
 					React.createElement(
--- bundle.js
+++ bundle.js
@@ -1,32 +1,34 @@
-require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"react-conditions":[function(require,module,exports){
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
 var React = require('react');
 
-//var WithKeyedChildren = require('./mixins/WithKeyedChildren');
+var TestEvaluator = require('../mixins/TestEvaluator');
+var WithKeyedChildren = require('../mixins/WithKeyedChildren');
 
 var Conditional = React.createClass({
 	displayName: 'Conditional',
 
-	_findChildByType: function _findChildByType(type) {
-		return this.props.children.find(function (child) {
-			return child.type === type;
-		});
-	},
+	mixins: [TestEvaluator, WithKeyedChildren],
 
 	_findChildToRender: function _findChildToRender() {
+		var _this = this;
+
 		var childrensCount = React.Children.count(this.props.children);
-		if (childrensCount === 0) return null;else if (childrensCount === 1) {
+		if (childrensCount === 0) return null;
+
+		if (childrensCount === 1) {
 			var child = this.props.children;
-			var testVal = typeof child.props.test === 'function' ? child.props.test() : child.props.test;
+			var testVal = this.evaluateTestProp(child.props);
 			return child.type === Conditional.If && testVal ? child : null;
 		} else {
 			var validChild = this.props.children.find(function (child) {
-				var testVal = typeof child.props.test === 'function' ? child.props.test() : child.props.test;
+				var testVal = _this.evaluateTestProp(child.props);
 				return (child.type === Conditional.If || child.type === Conditional.ElseIf) && testVal;
 			});
+
 			if (validChild) return validChild;else {
-				var elseComponent = this._findChildByType(Conditional.Else);
+				var elseComponent = this.findChildByType(Conditional.Else);
 				return elseComponent ? elseComponent : null;
 			}
 		}
@@ -85,4 +87,75 @@ Conditional.Else = React.createClass({
 module.exports = Conditional;
 
 
-},{"react":undefined}]},{},[]);
+},{"../mixins/TestEvaluator":3,"../mixins/WithKeyedChildren":4,"react":undefined}],2:[function(require,module,exports){
+'use strict';
+
+var React = require('react');
+
+var TestEvaluator = require('../mixins/TestEvaluator');
+
+var If = React.createClass({
+	displayName: 'If',
+
+	mixins: [TestEvaluator],
+
+	propTypes: {
+		test: React.PropTypes.oneOfType([React.PropTypes.bool.isRequired, React.PropTypes.func.isRequired])
+	},
+
+	render: function render() {
+		var testResult = this.evaluateTestProp();
+		var childrenCount = React.Children.count(this.props.children);
+		if (childrenCount === 0 || !testResult) return null;
+
+		if (childrenCount === 1 && React.isValidElement(this.props.children)) return this.props.children;else return React.createElement(
+			'div',
+			null,
+			this.props.children
+		);
+	}
+});
+
+module.exports = If;
+
+
+},{"../mixins/TestEvaluator":3,"react":undefined}],3:[function(require,module,exports){
+'use strict';
+
+module.exports = {
+	evaluateTestProp: function evaluateTestProp() {
+		var props = arguments.length <= 0 || arguments[0] === undefined ? this.props : arguments[0];
+
+		return typeof props.test === 'function' ? props.test() : props.test;
+	}
+};
+
+
+},{}],4:[function(require,module,exports){
+'use strict';
+
+module.exports = {
+	findChildByType: function findChildByType(type) {
+		return this.props.children.find(function (child) {
+			return child.type === type;
+		});
+	},
+
+	findChildrenByType: function findChildrenByType(type) {
+		return this.props.children.filter(function (child) {
+			return child.type === type;
+		});
+	}
+};
+
+
+},{}],"react-conditions":[function(require,module,exports){
+'use strict';
+
+module.exports = {
+	Conditional: require('./components/Conditional'),
+	If: require('./components/If')
+};
+
+
+},{"./components/Conditional":1,"./components/If":2}]},{},[]);
